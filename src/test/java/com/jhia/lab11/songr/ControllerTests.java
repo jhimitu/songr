50d1ee7fd65ccffe8952441cc4f17e73c6d31ee3
Jhia Turner <jhimitu@gmail.com>
2019-06-12 08:42:52 -0700

    write test for helllo world route

--- src/test/java/com/jhia/lab11/songr/ControllerTests.java
+++ src/test/java/com/jhia/lab11/songr/ControllerTests.java
@@ -1,4 +1,14 @@
 package com.jhia.lab11.songr;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class ControllerTests {
+    @Test
+    public void testHelloWorld() {
+        Controller controller = new Controller();
+
+        assertEquals("Hello, world!", controller.getHelloWorld());
+    }
 }
