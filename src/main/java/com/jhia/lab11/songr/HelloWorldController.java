1839fd3fe45d2ec320f7f09231f19cc7859c24fc
Jhia Turner <jhimitu@gmail.com>
2019-06-12 08:26:44 -0700

    implement a route that turns words into uppercase

--- src/main/java/com/jhia/lab11/songr/HelloWorldController.java
+++ src/main/java/com/jhia/lab11/songr/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.jhia.lab11.songr;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,10 @@ public class HelloWorldController {
     @GetMapping("/hello")
     public String getHelloWorld() {
         return "Hello, world!";
+    }
+
+    @GetMapping("/capitalize/{param}")
+    public String getCapitalizedString(@PathVariable String param) {
+        return param.toUpperCase();
     }
 }
